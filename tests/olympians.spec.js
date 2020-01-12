37a5630adc7654e2842eb4e3305bffc176ae78f0
Joshua Sherwood <jkjgsherwood@gmail.com>
2020-01-11 20:18:25 -0700

    Create test for olympians endpoint

---
+++ tests/olympians.spec.js
@@ -0,0 +1,47 @@
+var shell = require('shelljs');
+var request = require("supertest");
+var app = require('../app');
+
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+describe('test olympians path for get all request', () => {
+  beforeEach(async () => {
+    await database.raw('truncate table olympians cascade');
+
+    await database('olympians').insert([
+      {name: 'Denis Mikhaylovich Ablyazin', sex: 'M', age: '24', height: '161', weight: '62', team: 'Russia', games: '2016 Summer,Gymnastics', sport: "Gymnastics", event: "Men's Horse Vault", medal: "Silver"},
+      {name: 'Denis Mikhaylovich Ablyazin', sex: 'M', age: '24', height: '161', weight: '62', team: 'Russia', games: '2016 Summer,Gymnastics', sport: "Gymnastics", event: "Men's Floor", medal: "Bronze"},
+      {name: 'Elena Maria', sex: 'M', age: '24', height: '161', weight: '62', team: 'Brazil', games: '2016 Summer,Gymnastics', sport: "Running", event: "400m", medal: NULL},
+      {name: 'Gabriel Domenshev', sex: 'M', age: '24', height: '161', weight: '62', team: 'Romania', games: '2016 Summer,Gymnastics', sport: "Swimming", event: "400m", medal: NULL },
+      {name: 'Benji Levi', sex: 'M', age: '24', height: '161', weight: '62', team: 'Israel', games: '2016 Summer,Gymnastics', sport: "Soccer", event: "Gold Medal Match", medal: "Gold"}
+    ]);
+  });
+
+  afterEach(() => {
+    database.raw('truncate table olympians cascade');
+  });
+
+  describe('test olympians GET', () => {
+    it('happy path', async () => {
+      const response = await request(app)
+        .get("/api/v1/olympians");
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body.length).toBe(4);
+
+      expect(response.body[0]).toHaveProperty('name');
+      expect(response.body[0]).toHaveProperty('team');
+      expect(response.body[0]).toHaveProperty('age');
+      expect(response.body[0]).toHaveProperty('sport');
+      expect(response.body[0]).toHaveProperty('total_medals_won');
+
+      expect(response.body[0].name).toBe('Denis Mikhaylovich Ablyazin');
+      expect(response.body[0].team).toBe('Russia');
+      expect(response.body[0].age).toBe('24');
+      expect(response.body[0].sport).toBe("Gymnastics");
+      expect(response.body[0].total_medals_won).toBe(2);
+    });
+  });
+});
