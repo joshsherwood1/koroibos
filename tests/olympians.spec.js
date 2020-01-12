1b0ce9cb2d72955912c05ce51ca980a17e12f5fe
Joshua Sherwood <jkjgsherwood@gmail.com>
2020-01-12 13:24:46 -0700

    Create test for get request for olympian stats

--- tests/olympians.spec.js
+++ tests/olympians.spec.js
@@ -11,11 +11,11 @@ describe('test olympians path for get all request', () => {
     await database.raw('truncate table olympians cascade');
 
     await database('olympians').insert([
-      {name: 'Denis Mikhaylovich Ablyazin', sex: 'M', age: '24', height: '161', weight: '62', team: 'Russia', games: '2016 Summer,Gymnastics', sport: "Gymnastics", event: "Men's Horse Vault", medal: "Silver"},
-      {name: 'Denis Mikhaylovich Ablyazin', sex: 'M', age: '24', height: '161', weight: '62', team: 'Russia', games: '2016 Summer,Gymnastics', sport: "Gymnastics", event: "Men's Floor", medal: "Bronze"},
-      {name: 'Elena Maria', sex: 'M', age: '24', height: '161', weight: '62', team: 'Brazil', games: '2016 Summer,Gymnastics', sport: "Running", event: "400m", medal: null},
-      {name: 'Gabriel Domenshev', sex: 'M', age: '24', height: '161', weight: '62', team: 'Romania', games: '2016 Summer,Gymnastics', sport: "Swimming", event: "400m", medal: null },
-      {name: 'Benji Levi', sex: 'M', age: '24', height: '161', weight: '62', team: 'Israel', games: '2016 Summer,Gymnastics', sport: "Soccer", event: "Gold Medal Match", medal: "Gold"}
+      {name: 'Denis Mikhaylovich Ablyazin', sex: 'M', age: 20, height: 40, weight: 60, team: 'Russia', games: '2016 Summer,Gymnastics', sport: "Gymnastics", event: "Men's Horse Vault", medal: "Silver"},
+      {name: 'Denis Mikhaylovich Ablyazin', sex: 'M', age: 20, height: 40, weight: 60, team: 'Russia', games: '2016 Summer,Gymnastics', sport: "Gymnastics", event: "Men's Floor", medal: "Bronze"},
+      {name: 'Elena Maria', sex: 'F', age: 25, height: 70, weight: 40, team: 'Brazil', games: '2016 Summer,Gymnastics', sport: "Running", event: "400m", medal: null},
+      {name: 'Gabriella Domenshev', sex: 'F', age: 30, height: 90, weight: 50, team: 'Romania', games: '2016 Summer,Gymnastics', sport: "Swimming", event: "400m", medal: null },
+      {name: 'Benji Levi', sex: 'M', age: 40, height: 60, weight: 80, team: 'Israel', games: '2016 Summer,Gymnastics', sport: "Soccer", event: "Gold Medal Match", medal: "Gold"}
     ]);
   });
 
@@ -38,6 +38,22 @@ describe('test olympians path for get all request', () => {
       expect(response.body["olympians"][0]).toHaveProperty('age');
       expect(response.body["olympians"][0]).toHaveProperty('sport');
       expect(response.body["olympians"][0]).toHaveProperty('total_medals_won');
+    });
+  });
+
+  describe('test GET olympians stats', () => {
+    it('happy path', async () => {
+      const response = await request(app)
+        .get("/api/v1/olympian_stats");
+        console.log(response.body)
+
+      expect(response.statusCode).toBe(200);
+
+      expect(response.body).toHaveProperty('olympian_stats');
+      expect(response.body["olympian_stats"]).toHaveProperty("total_competing_olympians");
+      expect(response.body["olympian_stats"]["total_competing_olympians"]).toBe(4);
+      expect(response.body["olympian_stats"]).toHaveProperty("average_weight:");
+      expect(response.body["olympian_stats"]).toHaveProperty("average_age:");
     });
   });
 });
