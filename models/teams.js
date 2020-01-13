0bcc685631306b9a255b0b3f42b63dfaa0b1c2cc
Joshua Sherwood <jkjgsherwood@gmail.com>
2020-01-13 11:01:43 -0700

    Update teams model function getteams to include promise that returns all olympians names who belong to a certain team

--- models/teams.js
+++ models/teams.js
@@ -8,6 +8,11 @@ class Teams {
 
   async getTeams() {
     const teams = await database('olympians').select('team').groupBy('team')
+    await Promise.all(teams.map(async (team) => {
+      const olympians = await database('olympians').where('team', team.team)
+                          .select('name').groupBy('name')
+      team["olympians"] = olympians
+    }));
     return teams
   }
 }
