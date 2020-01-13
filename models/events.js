18cef76689361cc9d5e5b46487e2c2439072ab19
Joshua Sherwood <jkjgsherwood@gmail.com>
2020-01-12 21:46:53 -0700

    Condense events variable in getevents function and create getmedalists function in events model

--- models/events.js
+++ models/events.js
@@ -9,13 +9,27 @@ class Events {
   async getEvents() {
     const sports = await database('olympians').select('sport').groupBy('sport')
     await Promise.all(sports.map(async (sport) => {
-      const events = await database('olympians').whereNotNull('event').whereNot('event', 'NULL').where('sport', sport.sport).select('event').groupBy('event')
+      const events = await database('olympians').whereNotNull('event')
+                          .whereNot('event', 'NULL').where('sport', sport.sport)
+                          .select('event').groupBy('event')
       sport["events"] = []
       await Promise.all(events.map(async (event) => {
         sport["events"].push(event.event)
       }));
     }));
     return sports
+  }
+
+  async getMedalists(id) {
+    const data = {}
+    const eventInfo = await database('events').where('id', id).select('name')
+    const medalists = await database('olympians')
+                      .where('event', eventInfo[0]["name"])
+                      .whereNotNull('medal').whereNot('medal', 'NULL')
+                      .select('name', 'team', 'age', 'medal')
+    data["event"] = eventInfo[0]["name"]
+    data["medalists"] = medalists
+    return data
   }
 }
 
